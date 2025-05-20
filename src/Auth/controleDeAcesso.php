46ca871bf146eaac1da1860511f2683d53340962
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-05-20 09:34:14 -0300

    atualiza banco de dados

---
+++ src/Auth/controleDeAcesso.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace Microblog\Auth;
+
+//sobre sessoes- no php sessão(session) é uma funcionalidade usada para controle de acesso e outras imformações que seja importante enquanto o navegador estiver aberto no site
+
+//exemplos: areas administrativas painel de controle/dashboard, area de cliente. areade aluno, etc.
+
+// nesta area o acesso se da atraves de agumas formas de autenticação.Exemplos: login/senha biometria, token, etc. 
