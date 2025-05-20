76e864bbec4d3ecbb7fb17066ed9fa47cf586724
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-05-20 09:54:09 -0300

    atualiza areas de segurança em login

--- src/Auth/controleDeAcesso.php
+++ src/Auth/controleDeAcesso.php
@@ -7,3 +7,28 @@ namespace Microblog\Auth;
 //exemplos: areas administrativas painel de controle/dashboard, area de cliente. areade aluno, etc.
 
 // nesta area o acesso se da atraves de agumas formas de autenticação.Exemplos: login/senha biometria, token, etc. 
+
+final class ControleDeAcesso
+{
+    private function __construct() {}
+
+    
+    public static function iniciarSessao(): void
+    {
+        if (!isset($_SESSION))session_start();
+    }
+
+
+
+    public function exigirLogin(): void
+    {   //inicia sessao (se necessario)
+        self::iniciarSessao();
+
+        //se nao existir uma variavel de sessao chamada id
+        if (!isset($_SESSION["id"])) {
+            session_destroy();
+            header("Location:../login.php?acesso_proibido");
+            exit();
+        }
+    }
+}
--- src/Database/ConexaoBD.php
+++ src/Database/ConexaoBD.php
@@ -15,7 +15,7 @@ abstract class ConexaoBD
     private static string $servidor = "localhost";
     private static string $usuario = "root";
     private static string $senha = "";
-    private static string $banco = "microblog_INICIAL";
+    private static string $banco = "microblog_marcio";
 
     public static function getConexao(): PDO
     {
