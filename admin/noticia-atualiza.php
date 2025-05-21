f29cfda550db8b77d286472966eebb01f6c05798
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-05-21 10:46:24 -0300

    programa noticia atualiza e exclui

--- admin/noticia-atualiza.php
+++ admin/noticia-atualiza.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../vendor/autoload.php";
 
+use Microblog\Auth\ControleDeAcesso;
 use Microblog\Enums\Destaque;
 use Microblog\Enums\TipoUsuario;
 use Microblog\Helpers\Utils;
@@ -9,12 +10,13 @@ use Microblog\Models\Noticia;
 use Microblog\Services\CategoriaServico;
 use Microblog\Services\NoticiaServico;
 
+ControleDeAcesso::exigirLogin();
 $idNoticia = Utils::sanitizar($_GET["id"], "inteiro");
 Utils::verificarId($idNoticia);
 
 // Configurar após programar Controle de Acesso
-$idUsuario = 1;
-$tipoUsuario = TipoUsuario::from('admin ou editor');
+$idUsuario = $_SESSION["id"];
+$tipoUsuario = TipoUsuario::from($_SESSION["tipo"]);
 
 
 $mensagemErro = "";
